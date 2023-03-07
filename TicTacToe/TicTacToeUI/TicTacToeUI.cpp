9778fafdcf27f4882f7f2af7ddef1f877f833a92
EneClaudiu <eneclaudiu02@gmail.com>
2023-03-07 11:51:10 +0200

    Start QT

--- TicTacToe/TicTacToeUI/TicTacToeUI.cpp
+++ TicTacToe/TicTacToeUI/TicTacToeUI.cpp
@@ -1,10 +1,61 @@
 #include "TicTacToeUI.h"
+#include "QMessageBox"
 
 TicTacToeUI::TicTacToeUI(QWidget *parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
+
+    this->setFixedSize(QSize(600, 400));
+    setWindowFlags(Qt::Dialog | Qt::MSWindowsFixedSizeDialogHint);
+    
+    ui.stackedWidget->setCurrentIndex(0);
+
+    player1 = "Player1";
+    player2 = "Player2";
+
 }
 
 TicTacToeUI::~TicTacToeUI()
 {}
+
+void TicTacToeUI::on_pushButton_player_clicked() {
+    if (ui.lineEdit_player1->text() != "")
+        player1 = ui.lineEdit_player1->text();
+    if(ui.lineEdit_player2->text()!="")
+        player2 = ui.lineEdit_player2->text();
+    game->SetPlayerNames(player1.toStdString(), player2.toStdString());
+    ui.stackedWidget->setCurrentIndex(1);
+    
+    ui.label->setText("Here will be some info text.");
+}
+
+void TicTacToeUI::on_pushButton_1_clicked() {
+    QMessageBox::information(this, "Clicked",QString::fromStdString(game->GetCurrentPlayer()));
+}
+void TicTacToeUI::on_pushButton_2_clicked() {
+
+}
+void TicTacToeUI::on_pushButton_3_clicked() {
+    
+}
+void TicTacToeUI::on_pushButton_4_clicked() {
+
+}
+void TicTacToeUI::on_pushButton_5_clicked() {
+    
+}
+void TicTacToeUI::on_pushButton_6_clicked() {
+    
+}
+void TicTacToeUI::on_pushButton_7_clicked() {
+
+}
+void TicTacToeUI::on_pushButton_8_clicked() {
+
+}
+void TicTacToeUI::on_pushButton_9_clicked() {
+
+}
+
+
--- TicTacToe/TicTacToeUI/TicTacToeUI.h
+++ TicTacToe/TicTacToeUI/TicTacToeUI.h
@@ -2,6 +2,7 @@
 
 #include <QtWidgets/QMainWindow>
 #include "ui_TicTacToeUI.h"
+#include "ITicTacToe.h"
 
 class TicTacToeUI : public QMainWindow
 {
@@ -13,4 +14,20 @@ public:
 
 private:
     Ui::TicTacToeUIClass ui;
+    QString player1;
+    QString player2;
+
+    ITicTacToePtr game = ITicTacToe::Produce(ETicTacToeGameType::Impl1);
+private slots:
+    void on_pushButton_player_clicked();
+
+    void on_pushButton_1_clicked();
+    void on_pushButton_2_clicked();
+    void on_pushButton_3_clicked();
+    void on_pushButton_4_clicked();
+    void on_pushButton_5_clicked();
+    void on_pushButton_6_clicked();
+    void on_pushButton_7_clicked();
+    void on_pushButton_8_clicked();
+    void on_pushButton_9_clicked();
 };
